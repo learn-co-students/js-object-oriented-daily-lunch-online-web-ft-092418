9b74e39c564ca62a1981fe26b51d5dcb94e40ce8
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-01-12 13:21:23 -0500

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ class Meal {
     this.price = price;
     store.meals.push(this);
   }
+  deliveries() {
+    return store.deliveries.filter(function(delivery) {return delivery.mealId === this.id}.bind(this))
+  }
+  customers() {
+    const deliveries = this.deliveries();
+    const customerIds = deliveries.map(function(delivery) {return delivery.customerId});
+    return store.customers.filter(function(customer) {return this.includes(customer.id)}.bind(customerIds))
+  }
+  static byPrice() {
+    return store.meals.sort(function(meal1, meal2) {
+      return meal2.price - meal1.price;
+    })
+  }
 }
 
 class Delivery {
@@ -48,6 +61,15 @@ class Customer {
     let mealId;
     return mealIds.map(function(mealId) {return store.meals.find(function(meal) {return meal.id === this}.bind(mealId))})
    }
+   totalSpent() {
+     let total = 0;
+     let meals = this.meals();
+     let meal;
+     for (meal in meals){
+       total += meals[meal].price;
+     }
+     return total;
+   }
 }
 
 class Neighborhood {
@@ -62,5 +84,11 @@ class Neighborhood {
   customers() {
     return store.customers.filter(function(customer) {
       return customer.neighborhoodId == this.id}.bind(this));
+  }
+  meals() {
+    const deliveries = store.deliveries.filter(function(delivery) {return delivery.neighborhoodId === this.id}.bind(this))
+    const mealIds = deliveries.map(function(delivery) {return delivery.mealId})
+    const meals = store.meals.filter(function(meal) {return this.includes(meal.id)}.bind(mealIds))
+    return meals;
   }
 }
